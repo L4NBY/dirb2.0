02d915190e5006273424d1f3be50e82b21d88895
Lanby <86391117+L4NBY@users.noreply.github.com>
2022-03-13 17:45:35 -0300

    Add files via upload

---
+++ dinr.py
@@ -0,0 +1,16 @@
+import requests
+with open('lista.txt', 'r') as kkk:
+    diretorio = kkk.readlines()
+
+try:    
+    
+    url = input('digite a URL :')
+    for dir in diretorio:
+        
+        oi = requests.get(f'{url}/{dir}')
+        status = oi.status_code
+        if status == 200:
+            print(f'diretorio encontrado \033[32m{oi.url}\033[0;0m')
+            print('tentando encontrar ....')
+except:
+    print('erro , tente digitar parâmetro certo ')
